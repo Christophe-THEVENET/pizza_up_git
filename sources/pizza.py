f9cd8ec2d1370bba4af15b41ee2b37c6fd79aef7
Christophe-THEVENET <christophethevenet2.0@gmail.com>
2021-07-22 17:08:27 +0200

    ajout sauce sur certaines pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -11,6 +11,7 @@ RECIPES = {
     'quattro fromagi': {
         'ingredients': {'mozzarella': 2, 'parmesan': 2, 'gorgonzola': 2, 'cheddar': 2, 'oregano': 3},
         'price': 12,
+        'sauce': 'tomato'
     },
     'crudo': {
         'ingredients': {'mozzarella': 2, 'ham': 3, 'basil': 3, 'mushroom': 3},
@@ -19,6 +20,7 @@ RECIPES = {
     'pugliese': {
         'ingredients': {'mozzarella': 4, 'oregano': 2, 'onion': 5, 'mushroom': 2},
         'price': 11,
+        'sauce': 'tomato'
     },
     'romana': {
         'ingredients': {'ham': 4, 'olive': 3, 'onion': 2, 'basil': 2},
@@ -27,6 +29,7 @@ RECIPES = {
     'original': {
         'ingredients': {'garlic': 5, 'pepper': 4, 'parmesan': 2},
         'price': 10,
+        'sauce': 'tomato'
     },
     'ultima': {
         'ingredients': {'gorgonzola': 4, 'pepper': 5, 'onion': 4, 'ham': 3},
